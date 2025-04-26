80c8c714537ebe49af5b910572145ca572984e2f
pandiyarajanbt <pandiyarajaniosde@gmail.com>
2025-04-26 19:32:03 +0530

    remove the unnecessary file and code

--- Office/urls.py
+++ Office/urls.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, re_path
+from management.views.task import TaskView
+from management.views.project import ProjectView, BusinessUnitView
+from users.views import LoginView, SignUpView, TestTokenView
+from management.views.profile import ProfileView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/", include('management.urls')),
-    path("api/auth/", include('users.urls')),
+    re_path('admin/', admin.site.urls),
+    re_path('api/auth/login/', LoginView.as_view(), name='login'),
+    re_path('api/auth/signup/', SignUpView.as_view(), name='signup'),
+    re_path('api/auth/test-token/', TestTokenView.as_view(), name='test-token'),
+    re_path('api/task/', TaskView.as_view(), name='create-task'),
+    re_path('api/task/update/<int:pk>/', TaskView.as_view(), name='update-task'),
+    re_path('api/task/delete/<int:pk>/', TaskView.as_view(), name='delete-task'),
+    re_path('api/task/get/<int:pk>/', TaskView.as_view(), name='get-task'),
+    re_path('api/task/search/', TaskView.as_view(), name='search-task'),
+    re_path('api/project/', ProjectView.as_view(), name='create-project'),
+    re_path('api/project/update/<int:pk>/', ProjectView.as_view(), name='update-project'),
+    re_path('api/project/delete/<int:pk>/', ProjectView.as_view(), name='delete-project'),
+    re_path('api/project/get/<int:pk>/', ProjectView.as_view(), name='get-project'),
+    re_path('api/project/search/', ProjectView.as_view(), name='search-project'),
+    re_path('api/profile/', ProfileView.as_view(), name='create-profile'),
+    re_path('api/profile/update/<int:pk>/', ProfileView.as_view(), name='update-profile'),
+    re_path('api/profile/delete/<int:pk>/', ProfileView.as_view(), name='delete-profile'),
+    re_path('api/profile/get/<int:pk>/', ProfileView.as_view(), name='get-profile'),
+    re_path('api/profile/search/', ProfileView.as_view(), name='search-profile'),
+    re_path('api/business-unit/', BusinessUnitView.as_view(), name='create-business-unit'),
+    re_path('api/business-unit/update/<int:pk>/', BusinessUnitView.as_view(), name='update-business-unit'),
+    re_path('api/business-unit/delete/<int:pk>/', BusinessUnitView.as_view(), name='delete-business-unit'),
+    re_path('api/business-unit/get/<int:pk>/', BusinessUnitView.as_view(), name='get-business-unit'),
+    re_path('api/business-unit/search/', BusinessUnitView.as_view(), name='search-business-unit'),
 ]
--- management/views/project.py
+++ management/views/project.py
@@ -41,3 +41,41 @@ class ProjectView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
     
         
+
+class BusinessUnitView(APIView):
+    def post(self, request):
+        business_units = BusinessUnitSerializer(data=request.data)
+        if business_units.is_valid():
+            business_units.save()
+            return Response(business_units.data, status=status.HTTP_201_CREATED)
+        return Response(business_units.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        business_units = BusinessUnit.objects.all()
+        business_units_serializer = BusinessUnitSerializer(business_units, many=True)
+        return Response(business_units_serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        business_unit = BusinessUnit.objects.get(pk=pk)
+        business_unit_serializer = BusinessUnitSerializer(business_unit, data=request.data)
+        if business_unit_serializer.is_valid():
+            business_unit_serializer.save()
+            return Response(business_unit_serializer.data, status=status.HTTP_200_OK)
+        return Response(business_unit_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        business_unit = BusinessUnit.objects.get(pk=pk)
+        business_unit.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def search(self, request):
+        business_units = request.data.get('name')
+        if business_units:
+            business_units = BusinessUnit.objects.filter(name__icontains=business_units)
+            serializer = BusinessUnitSerializer(business_units, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+
+
